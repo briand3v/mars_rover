a645754a0d9dd6c087db294205d6f2ca0063888e
bryan <supercrew3@gmail.com>
2017-10-11 21:29:38 +0200

    Just setting up my Github

---
+++ rover.js
@@ -0,0 +1,25 @@
+var myRover = {
+  position: [0,0], 
+  direction: 'N'
+};
+
+function goForward(rover) {
+  switch(rover.direction) {
+    case 'N':
+      rover.position[0]++
+      break;
+    case 'E':
+      rover.position[1]++
+      break;
+    case 'S':
+      rover.position[0]--
+      break;
+    case 'W':
+      rover.position[1]--
+      break;
+  };
+
+  console.log("New Rover Position: [" + rover.position[0] + ", " + rover.position[1] + "]")
+}
+
+goForward(myRover);
